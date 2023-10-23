061a7640e6c7ff5a5b6da90aadb66d71e8a0e122
YasmineAfifi <ynremotecoders@gmail.com>
2023-10-23 03:07:13 +0300

    register form validation

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
 
-const userIdInput = document.getElementById("userId");
-const userNameInput =document.getElementById("userName");
-if(localStorage.getItem("userId")!=null && localStorage.getItem("userName")!=null ){
-    const userId =   localStorage.getItem("userId");
-    const userName = localStorage.getItem("userName");
-    // set hidden value with local storage value
-    userNameInput.value = userName;
-    userIdInput.value = userId;
-}
+// const userIdInput = document.getElementById("userId");
+// const userNameInput =document.getElementById("userName");
+// if(localStorage.getItem("userId")!=null && localStorage.getItem("userName")!=null ){
+//     const userId =   localStorage.getItem("userId");
+//     const userName = localStorage.getItem("userName");
+//     // set hidden value with local storage value
+//     userNameInput.value = userName;
+//     userIdInput.value = userId;
+// }
 
 
 
@@ -37,5 +37,65 @@ if(localStorage.getItem("userId")!=null && localStorage.getItem("userName")!=nul
 
 
 
+const registerForm = document.getElementById("regiserForm");
+registerForm.addEventListener("submit",function(event){
+event.preventDefault();
+const name =document.getElementById("name").value;
+const email =document.getElementById("email").value;
+const password =document.getElementById("password").value;
+const nameError =document.getElementById("nameError");
+const emailError =document.getElementById("emailError");
+const passwordError =document.getElementById("passwordError");
+// validation for user name
+if(name.trim() == ""){
+    nameError.innerText ="Name Is Required";
+}else if(!(/^[a-zA-Z]+(\s[a-zA-Z]+)?$/).test(name)){
+    nameError.innerText ="Name accepts alphabets only";
 
+}else if(name.length<3){
+    nameError.innerText ="Name must be more than 3 char";
+}else if(name.length>20){
+    nameError.innerText ="Name must be less than 20 char";
+}else{
+    nameError.innerText ="";
+}
+
+
+// validation for email
+
+if(email.trim() == ""){
+    emailError.innerText ="Email Is Required";
+
+}else if(!((/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)).test(email)){
+    emailError.innerText ="Invalid E-mail";
+
+}else{
+    emailError.innerText ="";
+}
+
+// validation for password
+
+if(password.trim() == "")
+{
+    passwordError.innerText = "Password Is Required";
+}else if(password.length<6 || password.length>15){
+    passwordError.innerText = "Password must be between 6 to 15 char";
+
+}else{
+    passwordError.innerText = "";
+}
+// if there is no errors submit the form
+
+if(nameError.innerText ==""&&emailError.innerText =="" &&passwordError.innerText == ""){
+
+    registerForm.submit();
+}
+
+
+
+
+
+
+
+})
 
